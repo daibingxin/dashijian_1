a3b6a095aa49bf9b0e4eacb35492fc92ad5def06
Dai <1443137609@qq.com>
2020-10-08 15:56:57 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,20 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ""
+        }
+    }
+    // 3.拦截所有响应 判断用户认证信息
+    options.complete = function (res) {
+        console.log(res);
+        //判断 如果是身份认证失败 跳转回登录页面
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            //1.删除本地token
+            localStorage.removeItem('token');
+            //2.页面跳转
+            location.href = '/login.html'
+        }
+    }
 })
